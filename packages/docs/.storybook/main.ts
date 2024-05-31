55428da81b164300090aac307575f6bbf8d2a263
MuriloDSantos <murilo.nsantos17@hotmail.com>
2024-05-31 17:10:04 -0300

    Commit config domain

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType == 'PRODUCTION') {
+      config.base = "/murilo-design-system/"
+    }
+
+    return config;
+  }
 };
 export default config;
